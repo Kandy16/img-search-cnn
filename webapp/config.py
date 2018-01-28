d174fef719a0cc9f36fde436a026891463249156
99123123 <34240555+99123123@users.noreply.github.com>
2018-01-28 16:20:05 +0100

    Delete config.py
    local config updated. deleting

--- webapp/config.py
+++
@@ -1,5 +0,0 @@
-PATH = ""
-
-CAFFE_IMAGES_PATH = '/home/codingyoddha/wrok/caffee/dataset/images/'
-CAFFE_FC8_PATH = '/home/codingyoddha/wrok/caffee/features/fc8/'
-CAFFE_FC7_PATH = '/home/codingyoddha/wrok/caffee/features/fc7/'
