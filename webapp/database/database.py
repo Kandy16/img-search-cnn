2732d5877f418585e883c00b07470a817454ad6b
Madhu Rakhal Magar <rakhal.madhu@gmail.com>
2018-03-17 20:22:37 +0100

    added model and layar selction

--- webapp/database/database.py
+++ webapp/database/database.py
@@ -6,10 +6,6 @@ class Database(object):
 		pass
 
 	def fillin_database(self,db):
-		db.session.add(MachineLearningAlgorithm(name='knn' , prepared= False))
-		db.session.add(MachineLearningAlgorithm(name='cosine' , prepared= False))
-		db.session.commit()
-
 		# Now we fill in the models using the ENUMMODELS
 		for model in Models:
 			print(model.value)
@@ -20,17 +16,44 @@ class Database(object):
 			if (model.name == Models.bvlc_alexnet.name or model.name == Models.bvlc_reference_caffenet.name):
 				obj_neural_layer = NeuralLayer(name='fc7' , neural_network = obj_neural_model  , extracted= False)
 				db.session.add(obj_neural_layer)
+				obj_ml_knn = MachineLearningAlgorithm(name='knn' , prepared= False, mlalgorithms=obj_neural_layer)
+				obj_ml_cos = MachineLearningAlgorithm(name='cosine' , prepared= False, mlalgorithms=obj_neural_layer)
+				db.session.add(obj_ml_knn)
+				db.session.add(obj_ml_cos)
+				db.session.commit()
+
 				obj_neural_layer = NeuralLayer(name='fc8' , neural_network = obj_neural_model  , extracted= False)
 				db.session.add(obj_neural_layer)
+				obj_ml_knn = MachineLearningAlgorithm(name='knn' , prepared= False, mlalgorithms=obj_neural_layer)
+				obj_ml_cos = MachineLearningAlgorithm(name='cosine' , prepared= False, mlalgorithms=obj_neural_layer)
+				db.session.add(obj_ml_knn)
+				db.session.add(obj_ml_cos)
+				db.session.commit()
 
 			if (model.name == Models.bvlc_googlenet.name) :
 				obj_neural_layer = NeuralLayer(name='pool5/7x7_s1' , neural_network = obj_neural_model  , extracted= False)
 				db.session.add(obj_neural_layer)
+				obj_ml_knn = MachineLearningAlgorithm(name='knn' , prepared= False, mlalgorithms=obj_neural_layer)
+				obj_ml_cos = MachineLearningAlgorithm(name='cosine' , prepared= False, mlalgorithms=obj_neural_layer)
+				db.session.add(obj_ml_knn)
+				db.session.add(obj_ml_cos)
+				db.session.commit()
 
 			if (model.name == Models.finetune_flickr_style.name):
 				obj_neural_layer = NeuralLayer(name='fc7' , neural_network = obj_neural_model  , extracted= False)
 				db.session.add(obj_neural_layer)
+				obj_ml_knn = MachineLearningAlgorithm(name='knn' , prepared= False, mlalgorithms=obj_neural_layer)
+				obj_ml_cos = MachineLearningAlgorithm(name='cosine' , prepared= False, mlalgorithms=obj_neural_layer)
+				db.session.add(obj_ml_knn)
+				db.session.add(obj_ml_cos)
+				db.session.commit()
+
 				obj_neural_layer = NeuralLayer(name='fc8_flickr' , neural_network = obj_neural_model  , extracted= False)
 				db.session.add(obj_neural_layer)
+				obj_ml_knn = MachineLearningAlgorithm(name='knn' , prepared= False, mlalgorithms=obj_neural_layer)
+				obj_ml_cos = MachineLearningAlgorithm(name='cosine' , prepared= False, mlalgorithms=obj_neural_layer)
+				db.session.add(obj_ml_knn)
+				db.session.add(obj_ml_cos)
+				db.session.commit()
 
 		db.session.commit()
