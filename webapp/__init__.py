cb1ac12444eb4d0b360e1d80941e056bc2d6cae2
volk <florian.volk93@gmail.com>
2018-03-26 12:06:23 +0200

    Added evaluation and clustering changes

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,4 @@
-from app import manager
-if __name__ == '__main__':
-    # TODO: Please remove while going to production
-    manager.run()
+# from app import manager
+# if __name__ == '__main__':
+#     # TODO: Please remove while going to production
+#     manager.run()
